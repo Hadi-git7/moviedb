3d6458b13296f26e9455a83e2e5568659956e4ba
Hadi-git7 <abouhomeinh@gmail.com>
2022-12-26 16:28:37 +0200

    step 4

--- index.js
+++ index.js
@@ -12,6 +12,24 @@ app.get('/time', (req, res) => {
   res.json({status: 200, message: currentTime});
 });
 
+app.get(`/hello/:ID`, (req,res) =>{
+  const ID = req.params.ID;
+  res.json({status: 200, message : `Hello, ${ID}`})
+});
+
+app.get('/hello', (req, res) => {
+  res.json({status: 200, message: 'Hello, world'});
+});
+
+app.get('/search', (req, res) => {
+  const search = req.query.s;
+  if (search) {
+    res.json({status: 200, message: "ok", data: search});
+  } else {
+    res.status(500).json({status: 500, error: true, message: "you have to provide a search"});
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
